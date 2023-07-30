5971f20d3679d6bc9793f4956a14fdbecad54c03
lapinRagnar <lapinragnar@gmail.com>
2023-07-30 22:07:28 +0200

    ajout pages about et produit et gestion routes

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur commodi voluptate eaque, sequi repellendus architecto ullam. Odio, nemo reprehenderit!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde nisi provident perspiciatis quis nihil accusantium corporis quibusdam rerum accusamus.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p{
+    margin: 20px 0;
+  }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Detail Produit - {{ id }}</h2>
+  </div>
+</template>
+
+<script setup>
+  const {id} = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
